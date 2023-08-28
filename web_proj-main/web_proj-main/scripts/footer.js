ce318e3dc163ef048f7f847451302d05415354bc
Adi Talmor <aditalmor2@cs.colman.ac.il>
2023-08-28 14:13:01 +0300

    update
    fix

--- web_proj-main/web_proj-main/scripts/footer.js
+++
@@ -1,5 +0,0 @@
-fetch('footer.html')
-            .then(response => response.text())
-            .then(navHtml => {
-            document.querySelector('#footer-placeholder').innerHTML = navHtml;
-            });
